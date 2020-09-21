81f7db86c36022aad74f979e56f4b4ef709d3dc3
RLKhoury <rakanlkhoury@gmail.com>
2020-09-21 16:41:24 -0400

    Restructuring folders + Lab 2 folder

---
+++ Lab 1/helloworld.py
@@ -0,0 +1,2 @@
+print("Hello World - Rakan Khoury: 3 Years+PEY")
+
--- helloworld.py
+++
@@ -1,3 +0,0 @@
-print("Hello World - Rakan Khoury: 3 Years+PEY")
-print("Hello, this is Farah")
-print("This is Sam, chainging your py on Sept 17, 2020")
